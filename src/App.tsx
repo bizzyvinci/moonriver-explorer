52e014b958b9a11bf6b3689df41daba25560625f
pizzy <horlasehinde@gmail.com>
2021-12-13 09:14:50 +0100

    feat: Table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import WithSubnavigation from './components/Navbar'
+import { Box } from '@chakra-ui/react';
+import Navbar from './components/Navbar'
+import Table from './components/Table'
+
+const columns = [
+  {Header: 'Col-1', accessor: 'col-1'},
+  {Header: 'Col-2', accessor: 'col-2'},
+  {Header: 'Col-3', accessor: 'col-3'},
+]
+
+const data = [
+  {'col-1': 5, 'col-2': 2, 'col-3': 3},
+  {'col-1': 5, 'col-2': 2, 'col-3': 3},
+  {'col-1': 5, 'col-2': 2, 'col-3': 3}
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <WithSubnavigation />
-    </div>
+    <Box className="App">
+      <Navbar />
+      <Table data={data} columns={columns} />
+    </Box>
   );
 }
 
---
+++ src/components/Table.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+  Table as RTable,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from '@chakra-ui/react'
+import { useTable } from 'react-table'
+
+
+export default function Table({ columns, data, variant }: Props) {
+  // Use the state and functions returned from useTable to build your UI
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data,
+  })
+
+  // Render the UI for your table
+  return (
+    <RTable  variant={variant || 'striped'} {...getTableProps()}>
+      <Thead>
+        {headerGroups.map(headerGroup => (
+          <Tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <Th {...column.getHeaderProps()}>{column.render('Header')}</Th>
+            ))}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <Tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <Td {...cell.getCellProps()}>{cell.render('Cell')}</Td>
+              })}
+            </Tr>
+          )
+        })}
+      </Tbody>
+    </RTable>
+  )
+}
+
+
+export interface Column {Header: string; accessor: string}
+export interface Columns extends Array<Column> {}
+export interface SingleData { [index: string]: any; }
+export interface Data extends Array<SingleData> {}
+export interface Props {columns: Columns; data: Data; variant?: string}
