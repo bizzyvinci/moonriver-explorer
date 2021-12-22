2acbc7b296995ba62708a021793db63991f4fcd5
bizzy <horlasehinde@gmail.com>
2021-12-22 10:01:37 +0100

    chore: remove Config that's now ENDPOINT

--- src/utils.jsx
+++ src/utils.jsx
@@ -2,9 +2,6 @@ import { Link } from '@chakra-ui/react'
 import { CheckCircleIcon, CloseIcon } from '@chakra-ui/icons'
 import bigInt from 'big-integer'
 
-export const Config = {
-  endpoint: 'https://api.subquery.network/sq/bizzyvinci/another-moonriver__Yml6e'
-}
 
 export const PAGE_LIMIT = 50
 export const DECIMAL = 18
